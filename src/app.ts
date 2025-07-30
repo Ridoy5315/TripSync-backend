2c7390839f3c8e68a8d6010da58a7fdd36a9cf54
Ridoy5315 <ridoy.st99@gmail.com>
2025-07-30 12:47:16 +0800

    global error handleing has been completed

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,25 @@
-import express, { Request, Response } from "express";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import express, {  Request, Response } from "express";
+import { router } from "./app/routes";
+import cors from "cors"
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFoundRoute from "./app/middlewares/notFoundRoute";
 
 const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use("api", router)
 
 app.get("/", (req: Request, res: Response) => {
      res.status(200).json({
           message: "Welcome to TripSync(Backend)"
      })
 })
+
+app.use(globalErrorHandler)
+
+app.use(notFoundRoute)
 
 export default app;
---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,67 @@
+import z from "zod";
+import { Gender, IsActive, Role } from "./user.interface";
+
+export const createUserZodSchema = z.object({
+  name: z
+    .string({ error: "Name must be string" })
+    .min(4, { message: "Name must be at least 2 characters long." })
+    .max(50, { message: "Name cannot exceed 50 characters." }),
+  email: z
+    .string({ error: "Email must be string" })
+    .email({ message: "Invalid email address format" })
+    .min(10, { message: "Email must be at least 10 characters long." })
+    .max(60, { message: "Email cannot exceed 60 characters." }),
+  password: z
+    .string({ error: "Password must be string" })
+    .min(8, { message: "Password must be at least 8 characters long." })
+    .regex(/^(?=.*[A-Z])/, {
+      message: "Password must contain at least 1 uppercase letter.",
+    })
+    .regex(/^(?=.*[!@#$%^&*])/, {
+      message: "Password must contain at least 1 special character.",
+    })
+    .regex(/^(?=.*\d)/, {
+      message: "Password must contain at least 1 number.",
+    }),
+  phone: z
+    .string({ error: "Phone Number must be string" })
+    .regex(/^(?:\+8801[3-9]\d{8}|01[3-9]\d{8}|\+861[3-9]\d{9}|1[3-9]\d{9})$/, {
+      message: "Phone number must be valid for Bangladesh or China.",
+    })
+    .optional(),
+});
+
+export const updateUserZodSchema = z.object({
+  name: z
+    .string({ error: "Name must be string" })
+    .min(4, { message: "Name must be at least 4 characters long." })
+    .max(50, { message: "Name cannot exceed 50 characters." })
+    .optional(),
+  phone: z
+    .string({ error: "Phone Number must be string" })
+    .regex(/^(?:\+8801[3-9]\d{8}|01[3-9]\d{8}|\+861[3-9]\d{9}|1[3-9]\d{9})$/, {
+      message: "Phone number must be valid for Bangladesh or China.",
+    })
+    .optional(),
+  address: z
+    .string({ error: "Address must be string" })
+    .max(200, { message: "Address cannot exceed 200 characters." })
+    .optional(),
+  picture: z.string({ error: "Picture must be string" }).optional(),
+  dateOfBirth: z
+    .string({ error: "Date Of Birth must be string" })
+    .regex(/^\d{4}-\d{2}-\d{2}$/, {
+      message: "Invalid birth date format. Format: 2000-07-05.",
+    })
+    .optional(),
+  gender: z.enum(Object.values(Gender) as [string]).optional(),
+  monthlyCancelLimit: z
+    .number({ error: "Monthly cancel limitation must be number" })
+    .optional(),
+  isActive: z.enum(Object.values(IsActive) as [string]).optional(),
+  isDeleted: z.boolean({ error: "isDeleted must be true or false" }).optional(),
+  isVerified: z
+    .boolean({ error: "isVerified must be true or false" })
+    .optional(),
+  role: z.enum(Object.values(Role) as [string]).optional(),
+});
